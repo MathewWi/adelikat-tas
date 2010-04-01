78dec27e2601c01029a1c6598e4f292864b1d577
andrewjpritchard <andrewjpritchard@0bee6f1e-5ead-11de-8832-3f4c773c73ae>
2010-04-01 01:03:23 +0000

    Seeing as TASing is now useless, I decided to delete everything in the SVN. Hope you don't mind!

--- trunk/SMB3/smb3.bat
+++
@@ -1,5 +0,0 @@
-start fceux.exe -readonly 1 -pause 1 -playmovie smb3full.fm2 -loadstate .\fcs\smb3.smb3full.fc0 -lua smb3.lua smb3.nes
-cd .\Watch
-start fceux.exe -readonly 1 -pause 1 -playmovie smb3full-acdj.fm2 -loadstate .\fcs\smb3.smb3full-acdj.fc0 smb3.nes
-cd ..
-start SMB3.xls
