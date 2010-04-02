fe2833e96ea38a16735bb56d119e969f68d9ffe4
andrewjpritchard <andrewjpritchard@0bee6f1e-5ead-11de-8832-3f4c773c73ae>
2010-04-02 04:56:24 +0000

    SVN restored. Sorry adelikat :(

---
+++ DW4/luaScripts/DW4_BattleRound-old.lua
@@ -0,0 +1,136 @@
+--DW4_Get_Encounter
+--Start on frame --7076.
+Turn = 0x96;
+
+
+
+
+CHECK = savestate.create();
+local buttonmap = {[1]='up',[2]='down',[4]='left',[8]='right',[16]='A',[32]='B',[64]='start',[128]='select'} 
+
+
+
+
+FCEU.frameadvance();
+
+
+framefind = savestate.create();
+key1 = {};
+done = false;
+FCEU.speedmode('turbo');
+savestate.save(CHECK);
+while not done do
+--7076 to 7201 
+	savestate.load(CHECK);
+	control = math.random(0,255);
+	for bit,button in pairs(buttonmap) do
+    		if AND(control,bit) ~= 0 then
+      			key1[button]=true;
+      		else
+      			key1[button]=false;
+	    	end    
+  		end
+  		joypad.set(1,key1) 
+  		for i = 1,15,1 do   	  	
+  			FCEU.frameadvance();  
+		end;
+	   control = math.random(1,255);
+	    for bit,button in pairs(buttonmap) do
+    		if AND(control,bit) ~= 0 then
+      			key1[button]=true;
+      		else
+      			key1[button]=false;
+	    	end    
+  		end
+  		joypad.set(1,key1) 
+  		for i = 1,26,1 do   	  	
+  			FCEU.frameadvance();  
+		end;
+		
+		control = math.random(0,1)*64 + 16;
+		for bit,button in pairs(buttonmap) do
+    		if AND(control,bit) ~= 0 then
+      			key1[button]=true;
+      		else
+      			key1[button]=false;
+	    	end    
+  		end
+  		--Attack
+  		joypad.set(1,key1) 
+  		for i = 1,5,1 do   	  	
+  			FCEU.frameadvance();  
+		end;
+		control = 1;
+		for bit,button in pairs(buttonmap) do
+    		if AND(control,bit) ~= 0 then
+      			key1[button]=true;
+      		else
+      			key1[button]=false;
+	    	end    
+  		end
+  		--Up
+  		joypad.set(1,key1)   		
+  		FCEU.frameadvance();  	  		
+		control = math.random(0,3)*64 + 16;
+		for bit,button in pairs(buttonmap) do
+    		if AND(control,bit) ~= 0 then
+      			key1[button]=true;
+      		else
+      			key1[button]=false;
+	    	end    
+  		end
+  		--Select
+  		joypad.set(1,key1) 
+  		fz =  1;		
+  		for i = 1,3+fz,1 do   	  	
+  			FCEU.frameadvance();  
+		end;
+		control = math.random(0,1)*64 + 16;
+		for bit,button in pairs(buttonmap) do
+    		if AND(control,bit) ~= 0 then
+      			key1[button]=true;
+      		else
+      			key1[button]=false;
+	    	end    
+  		end
+  		--Ragn
+  		joypad.set(1,key1) 
+  		for i = 1,9,1 do   	  	
+  			FCEU.frameadvance();  
+		end;
+		control = math.random(0,255);
+		for bit,button in pairs(buttonmap) do
+    		if AND(control,bit) ~= 0 then
+      			key1[button]=true;
+      		else
+      			key1[button]=false;
+	    	end    
+  		end
+       --Lagless  			
+  		for i = 1,3,1 do   	  	
+  			joypad.set(1,key1) 
+  			FCEU.frameadvance();  
+		end;
+		FCEU.frameadvance();  
+		while FCEU.lagged() == true do
+			FCEU.frameadvance();  
+		end;
+		
+			FCEU.frameadvance();  		
+	    control = math.random(1,255);
+	    for bit,button in pairs(buttonmap) do
+    		if AND(control,bit) ~= 0 then
+      			key1[button]=true;
+      		else
+      			key1[button]=false;
+	    	end    
+  		end
+  		joypad.set(1,key1) 
+  		for i = 1,10,1 do   	  	
+  			FCEU.frameadvance();  
+		end;
+		if memory.readbyte(0x7361) > 20 then
+			done = true;
+		end;
+end;
+					
---
+++ SMB3/smb3.lua
@@ -0,0 +1,62 @@
+--SMB3 lua script
+--Written by adelikat
+--Used to identify and measure corner boost savings
+
+local oldx = 0
+local oldxsub = 0
+local oldspeed = 0
+
+local newx = 0
+local newxsub = 0
+local newspeed = 0
+
+local predx = 0
+local predxsub = 0
+
+local totalboost = 0
+
+local lastframe = 0
+local currentframe = 0
+while true do
+
+lastframe = currentframe
+currentframe = emu.framecount()
+
+if (currentframe < lastframe) then	--Reset the boost counter if a state is loaded
+totalboost = 0
+end
+
+x = input.get()
+if (x.R) then			--Change R to whatever key you like for this hotkey
+totalboost = 0
+end
+
+oldx = newx
+oldxsub = newxsub
+oldspeed = newspeed
+
+newspeed = memory.readbyte(0x00BD)
+if (newspeed > 128) then
+newspeed = newspeed - 256
+end
+
+newx = memory.readbyte(0x0090)
+newxsub = memory.readbyte(0x074D)
+
+pos = "Position: (" .. string.format('%3d',newspeed) .. ") " .. string.format('%3d',newx) .. " " .. string.format('%2d',newxsub/16) .. "/16"
+
+--Predict newx
+
+predx = oldx + math.floor(((oldspeed*16 + oldxsub)/256))
+predxsub = (oldxsub + (oldspeed*16))%256
+
+if (oldspeed > 1 and newx-predx > 0 and (newx-predx < 3)  )then  -- if boost is 3 or greater it must be erroneous results, ignore
+totalboost = totalboost + (newx - predx)
+end
+
+gui.text(1,1,"Total boost: " .. totalboost)
+gui.text(124,1,"Predict:       " .. string.format('%d',predx) .. " " .. string.format('%2d', predxsub/16) .. "/16" )
+gui.text(126,13,pos)
+
+emu.frameadvance()
+end
