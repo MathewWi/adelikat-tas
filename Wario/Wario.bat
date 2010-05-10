eac493b00e9b50fb8dd6d00ade708e05fba81628
andres.delikat <andres.delikat@0bee6f1e-5ead-11de-8832-3f4c773c73ae>
2010-05-10 13:17:35 +0000

    Wario - more restructuring

--- Wario/Wario.bat
+++ Wario/Wario.bat
@@ -1,4 +1,4 @@
-start VBjin.exe
+start VBjin.exe -rom D:\TAS\adelikatTAS\Wario\Wario.vb -play D:\TAS\adelikatTAS\Wario\wario-full.mc2 -pause 1
 start Wario.xlsx
-cd .\v1
-start VBjin.exe 
+cd .\v2
+start VBjin.exe -rom D:\TAS\adelikatTAS\Wario\v2\Wario.vb -play D:\TAS\adelikatTAS\Wario\v2\adelikatv2-wario.mc2 -pause 1
