025ca0848dfbd443701fa79157fb1308a82113bb
andres.delikat <andres.delikat@0bee6f1e-5ead-11de-8832-3f4c773c73ae>
2010-07-29 22:07:42 +0000

    TMNT - Enter building

--- trunk/TMNT/TMNT.bat
+++ trunk/TMNT/TMNT.bat
@@ -1,5 +1,5 @@
 start fceux.exe -pause 1 -readonly 1 -playmovie tmntv5.fm2 -loadstate .\fcs\tmnt.tmntv5.fc0 tmnt.nes
 start TMNT.xls
 cd .\Watch
-start fceux.exe -pause 1 -readonly 1 -playmovie tmntv4.fm2 -loadstate .\fcs\tmnt.tmntv4-tmnt.fc0 tmnt.nes
+start fceux.exe -pause 1 -readonly 1 -playmovie tmntv4.fm2 -loadstate .\fcs\tmnt.tmntv4.fc0 tmnt.nes
 explorer.exe D:\TAS\adelikatTAS\TMNT
