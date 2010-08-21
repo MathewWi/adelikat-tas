f3983d3e59f61f10e4f4b42592aaf2036a287647
andres.delikat <andres.delikat@0bee6f1e-5ead-11de-8832-3f4c773c73ae>
2010-08-21 19:03:19 +0000

    Terminator - small fix to bat, this is a near useless commit

--- trunk/Terminator/Terminator.bat
+++ trunk/Terminator/Terminator.bat
@@ -1,5 +1,5 @@
 start gens.exe Terminator.iso -play TERMINATORv2.gmv -loadstate ".\Savestates\THE TERMINATOR - TERMINATORv2.gs0" -pause 1
 start TerminatorSegaCD.xlsx
 cd .\Watch
-start gens.exe Terminator.iso -play TERMINATORv1.gmv -loadstate ".\Savestates\THE TERMINATOR - TERMINATORv1.gs0" -pause 1
+start gens.exe ..\Terminator.iso -play TERMINATORv1.gmv -loadstate ".\Savestates\THE TERMINATOR - TERMINATORv1.gs0" -pause 1
 explorer.exe D:\TAS\adelikatTAS\Terminator
