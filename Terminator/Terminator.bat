46d99e85d94e65b184de3137ec08cc5400f0c6e8
andres.delikat <andres.delikat@0bee6f1e-5ead-11de-8832-3f4c773c73ae>
2010-09-09 18:49:17 +0000

    Move Terminator to archive

--- Terminator/Terminator.bat
+++
@@ -1,5 +0,0 @@
-start gens.exe Terminator.iso -play TERMINATORv2.gmv -loadstate ".\Savestates\THE TERMINATOR - TERMINATORv2.gs0" -pause 1
-start TerminatorSegaCD.xlsx
-cd .\Watch
-start gens.exe ..\Terminator.iso -play TERMINATORv1.gmv -loadstate ".\Savestates\THE TERMINATOR - TERMINATORv1.gs0" -pause 1
-explorer.exe "C:\Documents and Settings\Owner\My Documents\My Dropbox\TheTerminator\"
