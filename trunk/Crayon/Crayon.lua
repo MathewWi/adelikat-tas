71b70aeeb323f754832a465588251d47603647b1
andres.delikat <andres.delikat@0bee6f1e-5ead-11de-8832-3f4c773c73ae>
2010-09-19 02:12:21 +0000

    Add files for Crayon Shin-Chan 4 even though I'm 70% done at this point.

---
+++ trunk/Crayon/Crayon.lua
@@ -0,0 +1,36 @@
+local PixAddr = 0xFFB1
+local subPixAddr = 0xFFB0
+
+local currDistance = 0;
+local lastDistance = 0;
+local currSpeed = 0;
+local lostCounter = 0;
+
+while true do
+--todo: pix addr is 2 bytes! account for that
+--input.get for a reset lostCounter
+lastDistance = currDistance
+currDistance = ( (memory.readbyte(PixAddr) * 16) + (memory.readbyte(subPixAddr) / 16) )
+currSpeed = currDistance - lastDistance
+
+if (currSpeed < 14) then
+lostCounter = lostCounter + (14 - currSpeed)
+end
+
+print(currSpeed .. " " .. lostCounter)
+gui.text(10,10,currSpeed .. "  " .. lostCounter)
+
+
+--Squirrel Spam
+buttons = input.get()
+
+if (buttons.L == true) then
+	if (emu.framecount() % 4 == 0) then
+		joypad.set(1,{A=true, right=true})
+	end
+end
+
+
+
+emu.frameadvance()
+end
