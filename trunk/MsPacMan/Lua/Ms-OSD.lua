962b5890eb4ddefd835e2ba15089127d2382fae8
adelikat@tasvideos.org <adelikat@tasvideos.org@0bee6f1e-5ead-11de-8832-3f4c773c73ae>
2015-01-09 01:25:35 +0000

    Ms PacMan - track move counter across save/loadstate and add a reset button

--- trunk/MsPacMan/Lua/Ms-OSD.lua
+++ trunk/MsPacMan/Lua/Ms-OSD.lua
@@ -4,6 +4,31 @@ xaddr = 0x0103
 leveladdr = 0x00A6;
 moveCounter = 0;
 
+prevMoveCounters = { }
+
+function DrawMoveCounter()
+	gui.text(0, 0, "Moves: " .. moveCounter, null, null, 1);
+end
+
+function OnSaveState(name)
+	prevMoveCounters[name] = moveCounter;
+end
+
+function OnLoadState(name)
+	if (prevMoveCounters[name] ~= nil) then
+		moveCounter = prevMoveCounters[name];
+	else
+		moveCounter = -1;
+		console.log("Could not find counter for this state")
+	end
+
+	gui.cleartext();
+	DrawMoveCounter();
+end
+
+event.onsavestate(OnSaveState, "OSD-OnSavestate");
+event.onloadstate(OnLoadState, "OSD-OnLoadstate");
+
 memory.usememorydomain("RAM2");
 while true do
 	prev_xval = xval;
@@ -24,7 +49,13 @@ while true do
 		moveCounter = 0;
 	end
 
-	gui.text(0, 0, "Moves: " .. moveCounter, null, null, 1);
+	pressed = input.get();
+	if (pressed["End"]) then
+		moveCounter = 0;
+	end
+
+	DrawMoveCounter();
 
 	emu.frameadvance();
 end
+
